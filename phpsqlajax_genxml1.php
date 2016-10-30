5213acd2f6211aa762012d6f0b1dbc8f440b88ea
Mohammed Khursheed Ali Khan <khursheedali38@gmail.com>
2016-10-30 17:35:36 +0530

    Initial commit
    Added Google Maps API (i.e) integrating it with wordpress

---
+++ phpsqlajax_genxml1.php
@@ -0,0 +1,49 @@
+<?php
+
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+require_once('../../../wp-config.php');
+// Select all the rows in the markers table
+ global $wpdb ;
+ $query = $wpdb->get_results("SELECT * FROM `markers` ;") ;
+ /*echo "<table>";             
+  foreach($query as $q){
+  echo "<tr>";
+  //echo "<td>".date('Y-m-d h:i:s',$w->time)."</td>"; to convert unix epoch time to ist
+  echo "<td>".$q->name."</td>";
+  echo "<td>".$q->address."</td>";
+  echo "<td>".$q->type."</td>";
+  echo "</tr>";
+  }
+  echo "</table>";*/
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo '<markers>';
+
+// Iterate through the rows, printing XML nodes for each
+foreach ($query as $q) 
+  {
+  // ADD TO XML DOCUMENT NODE
+  echo '<marker ';
+  echo 'name="' . parseToXML($q->name) . '" ';
+  echo 'address="' . parseToXML($q->address) . '" ';
+  echo 'lat="' . $q->lat . '" ';
+  echo 'lng="' . $q->lng . '" ';
+  echo 'type="' . $q->type . '" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
+?>
